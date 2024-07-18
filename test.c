791f8da59d2b0e59a6209492fdd8791d8888e127
Ahmed Mousa <ahmed.mousa@vehiclevo.com>
2024-07-18 15:55:51 +0300

    New comments

--- test.c
+++ test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main ()
 {
-	printf("hehehe");
-  //comment added
-  // Comm1
-  // comm2  
+	printf("comm ver1"); //ver1 
+  //comment added ver1
+  // Comm1 ver1
+  // comm2 ver1 
   return 0;
 }

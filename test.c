7748dc4e93800fe5323c1cba653c9138d11e21de
Yasmin Amr <yasmin.amr@vehiclevo.com>
2024-07-18 15:55:49 +0300

    Yasmin's Commit

--- test.c
+++ test.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+
+int add(int x,int y)
+{
+	return x+y;
+}
+
 int main ()
 {
 	printf("hehehe");
   //comment added
   // Comm1
-  // comm2  
+  // comm2 
+   printf("Git Session");
+   int z= add(3,4)
+     
   return 0;
 }
